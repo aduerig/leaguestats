baf83548e310446472484690a6f95f0ad61e85d6
Ryan Quinn <fowardingtoyou@gmail.com>
2015-07-09 22:30:04 -0400

    Implemented code to input matchDetail and one relation underneath

--- filler.py
+++ filler.py
@@ -6,6 +6,8 @@ from sqlalchemy import *
 class Filler:
     def __init__(self):
         self.engine = create_engine("sqlite:///test.db")
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
         Base.metadata.create_all(self.engine, checkfirst=True)
 
     def helloWorld(self):
