986fed1b3f541261139902c63fa2b34fa079cad7
Andrew Duerig <gamechefproductions@gmail.com>
2015-07-14 23:32:09 -0400

    Added different regions to the api calls

--- riotApi.py
+++ riotApi.py
@@ -10,26 +10,32 @@ logger = logging.getLogger('leaguestats.riotApi')
 
 
 class RiotApi:
-    def __init__(self, api_key):
+    def __init__(self, api_key, region='na'):
         self.myKey = api_key
+        self.region = region
         self.dataC = {}
         self.dataT = {}
 
+
     def stopwatch(self, seconds):
-        print('Waiting 10 seconds to prevent request overflow...')
+        print('Waiting 1.3 seconds to prevent request overflow...')
         time.sleep(seconds)
         return
 
-    def getlink(self, url, name, path):
+    def getlink(self, url, name):
         self.stopwatch(1.3)
         print(name.encode('utf-8'))
         data = json.loads(requests.get(url + self.myKey).text)
         return data
 
+    def formaturl(self, url):
+        url = url.format(self.region)
+        return url
+
     def getchallenger(self):
-        urlchallenger = 'https://na.api.pvp.net/api/lol/na/v2.5/league/challenger' \
+        urlchallenger = 'https://{0}.api.pvp.net/api/lol/{0}/v2.5/league/challenger' \
                         '?type=RANKED_TEAM_5x5&api_key='
-        self.dataC = self.getlink(urlchallenger, 'Challenger Teams', '/Challenger Stats/')
+        self.dataC = self.getlink(self.formaturl(urlchallenger), 'Challenger Teams')
         logger.info('Successfully received data for all challenger teams')
         return self.dataC
 
@@ -38,11 +44,10 @@ class RiotApi:
             if name == x['playerOrTeamName']:
                 teamid = str(x['playerOrTeamId'])
                 teamname = str(x['playerOrTeamName']).replace('\ufb01', '')
-                urlteam = 'https://na.api.pvp.net/api/lol/na/v2.4/team/' + teamid \
+                urlteam = 'https://{0}.api.pvp.net/api/lol/{0}/v2.4/team/' + teamid \
                                 + '?api_key='
-                self.dataT = self.getlink(urlteam, teamname
-                                     + ' Match History', '/Challenger Stats/'
-                                     + teamname + '/')  # Match history of team
+                self.dataT = self.getlink(self.formaturl(urlteam), teamname
+                                     + ' Match History')  # Match history of team
                 break
         return self.dataT[teamid]
 
@@ -53,9 +58,8 @@ class RiotApi:
                 if match == str(x['gameId']):
                     gameid = str(x['gameId'])
                     gamename = str('Match ' + gameid).replace('\ufb01', '')
-                    urlmatch = 'https://na.api.pvp.net/api/lol/na/v2.2/match/' + gameid \
+                    urlmatch = 'https://{0}.api.pvp.net/api/lol/{0}/v2.2/match/' + gameid \
                                + '?includeTimeline=true' + '&api_key='
-                    data = self.getlink(urlmatch, gamename, '/Challenger Stats/' +
-                                        teamname + '/' + gameid + '/')  # Game of a team
+                    data = self.getlink(self.formaturl(urlmatch), gamename)  # Game of a team
                     break
         return data
